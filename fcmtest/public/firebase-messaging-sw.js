4358e02c1ded55d9ef1c8e807fb98405c1365e33
Alejandro Tapias <tapiaspinzon.5@gmail.com>
2022-01-25 09:28:32 -0500

    Agregado el Readme

--- fcmtest/public/firebase-messaging-sw.js
+++ fcmtest/public/firebase-messaging-sw.js
@@ -2,6 +2,7 @@
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
+// Config del proyecto
 const firebaseConfig = {
   apiKey: "AIzaSyANsokTJLSPb_yJ9484JfoGESh_1eDueQw",
   authDomain: "fcm-test-36ab9.firebaseapp.com",
@@ -11,19 +12,23 @@ const firebaseConfig = {
   appId: "1:622382328481:web:a04da539dbde34a0005ee0"
 };
 
+// Inicializar firebase
 firebase.initializeApp(firebaseConfig);
 
-
+// instancia dela funcion messaging
 const messaging = firebase.messaging();
 
+// esta funcion escucha las notificaciones cuando la app esta en segundo plano.
 messaging.onBackgroundMessage(function(payload) {
   console.log('Received background message ', payload);
  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
+    url: payload.notification.url,
   };
 
+  // Muestra la notificacion
   self.registration.showNotification(notificationTitle,
     notificationOptions);
 });
--- fcmtest/src/components/Notification.jsx
+++ fcmtest/src/components/Notification.jsx
@@ -5,27 +5,37 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Notification = () => {
 
-  const [notification, setNotification] = useState({title: '', body: ''});
+  // Se crea un usestate para almacenar el contenido de la notificacion.
+  const [notification, setNotification] = useState({title: '', body: '', url:''});
+
+  // Se crea la forma de la notificacion con la librearia react-toastify
   const notify = () =>  toast(<ToastDisplay/>);
   function ToastDisplay() {
     return (
       <div>
         <p><b>{notification?.title}</b></p>
         <p>{notification?.body}</p>
+        <p>{notification?.url}</p>
       </div>
     );
   };
 
+  // un useEffect que esta pendiente de cuando hay notificaciones nuevas.
   useEffect(() => {
     if (notification?.title ){
      notify()
     }
   }, [notification])
 
+  // Funcion para pedir permisos de enviarle notificaciones y generacion del token del navegador.
   requestForToken();
 
+  // Esta funcion esta pendiente de las nuevas notifiaciones
   onMessageListener().then((payload) => {
-    setNotification({title: payload?.notification?.title, body: payload?.notification?.body});     
+    setNotification({
+      title: payload?.notification?.title, 
+      body: payload?.notification?.body, 
+      url: payload?.notification?.url});     
   })
   .catch((err) => console.log('failed: ', err));
 
